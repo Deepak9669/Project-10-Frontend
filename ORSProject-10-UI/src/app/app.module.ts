aaff21e9e8a618a70b52c468ef74a0c78c6ee5a1
Deepak9669 <deepakverma726800@gmail.com>
2026-03-13 16:45:46 +0530

    done

---
+++ ORSProject-10-UI/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { EndpointServiceService } from './endpoint-service.service';
+import { HttpServiceService } from './http-service.service';  
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
